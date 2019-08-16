1086e166e1448c6c2df8d08d46ae3f158e7b10a7
Singhal-Vaibhav <vaibhav@scaledb.com>
2019-08-16 13:14:27 -0400

    plugin info version bumped

--- python/foglamp/plugins/south/openweathermap/openweathermap.py
+++ python/foglamp/plugins/south/openweathermap/openweathermap.py
@@ -88,7 +88,7 @@ def plugin_info():
 
     return {
         'name': 'OpenWeatherMap plugin',
-        'version': '1.5.0',
+        'version': '1.7.0',
         'mode': 'async',
         'type': 'south',
         'interface': '1.0',
